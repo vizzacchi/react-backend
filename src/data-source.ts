d6cd18ae8298e477af47e1183cf406a6b2fba7c6
Douglas Jorge Vizzacchi <douglas@vizzacchi.com.br>
2023-11-15 13:20:40 -0300

    Utilizando Variavel de Ambiente

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,15 +1,25 @@
 import "reflect-metadata"
-import { DataSource } from "typeorm"
+import { DataSource, DataSourceOptions } from "typeorm"
 import { User } from "./entity/User"
 import { Task } from "./entity/Task"
+import "dotenv/config";
+
+const getDBConfig = (json:any) => {
+    const newObject = {}
+    for (const prop in json){
+        if(!prop.startsWith("DB_")){
+            continue;
+        }
+        const newProp = prop.substring(prop.indexOf("_")+1).toLowerCase();
+        newObject[newProp] = json[prop];
+    }
+    return newObject;
+}
+
+const config = getDBConfig(process.env) as DataSourceOptions;
 
 export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: "motty.db.elephantsql.com",
-    port: 5432,
-    username: "enwkswla",
-    password: "yZMgF4XvkYUjCwPdrnFUjETW0oZKy79S",
-    database: "enwkswla",
+    ...config,
     synchronize: true,
     logging: false,
     entities: [User, Task],
